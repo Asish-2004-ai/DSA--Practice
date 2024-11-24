3605427ce75f0e95b5e82ff1940adb47e0601cd4
Asish-2004-ai <asishdalabehera375@gmail.com>
2024-11-24 10:24:44 +0530

    changed

---
+++ Aggresisve Cows/aggresivecows.cpp
@@ -0,0 +1,39 @@
+bool isposible(vector<int> &stalls, int k, int mid){
+    int cow = 1;
+    int last = stalls[0];
+    for(int i=0; i<stalls.size(); i++){
+        if(stalls[i]-last>=mid){
+            cow++;
+            if(cow==k){
+                return true;
+            }
+            last = stalls[i];
+        }
+    }
+    return false;
+}
+
+
+int aggressiveCows(vector<int> &stalls, int k)
+{   
+    sort(stalls.begin(), stalls.end());
+    int s = 0;
+    int maxi = -1;
+    for(int i=0;i<stalls.size();i++){
+        maxi = max(maxi, stalls[i]);
+    }
+    int e = maxi;
+    int ans = -1;
+    int mid = s +(e-s)/2;
+    while(s<=e){
+        if(isposible(stalls, k, mid)){
+            ans = mid;
+            s = mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+        mid = s +(e-s)/2;
+    }
+    return ans;
+}
